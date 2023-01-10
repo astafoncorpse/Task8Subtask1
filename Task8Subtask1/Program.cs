8038e4512e7c611626355a293c778533d69161b3
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-01-10 03:53:18 +0300

    Добавьте файлы проекта.

---
+++ Task8Subtask1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Task8Subtask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввидите адрес удаляемой папки");
+            string DirDirectory = Console.ReadLine();
+            if (Directory.Exists(DirDirectory))
+            {
+                var di = new DirectoryInfo(DirDirectory);
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    if ((DateTime.Now - file.LastAccessTime) > TimeSpan.FromMinutes(30))
+
+                    {
+                        file.Delete();
+                        Console.WriteLine("Файл {0} удален ", file.Name);
+                    }
+
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    if ((DateTime.Now - dir.LastAccessTime) > TimeSpan.FromMinutes(30))
+                    {
+                        dir.Delete(true);
+                        Console.WriteLine("Папка {0} удалена", dir.Name);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Папка не найдена. \n Введите корректный адрес папки");
+            }
+
+        }
+    }
+}
+
